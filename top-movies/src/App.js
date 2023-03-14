fa2f00715461a5d3898f73f9d93a0daef57c46ad
Juan Arcari <juansantos1995@hotmail.com>
2023-03-14 20:12:51 -0300

    import bootstrap

--- top-movies/src/App.js
+++ top-movies/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
@@ -21,15 +22,23 @@ export default function App() {
   var movies = movie;
 
   function showMovies(movies) {
-    return <div>
-      {movies.results.map(item => <p key={item.title}>{`${item.title}`}</p>)}
+    return <div className="table table-dark table-striped">
+      <thead className="headerTable">
+        <tr>
+          <th className="titleMovie" scope="col">Title</th>
+          <th scope="col">Description</th>
+        </tr>
+      </thead>
+      <tbody>
+          {movies.results.map(item => <tr key={item.title}><th className="titleMovie">{`${item.title}`}</th><td>{`${item.overview}`}</td></tr>)}
+      </tbody>
     </div>
   }
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1>The most watched movies now</h1>
+        <h1 className="title">The most watched movies currently</h1>
         <div>{showMovies(movies)}</div>
       </header>
     </div>
