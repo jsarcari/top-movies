d86fcd9e6b51176e8eee808d71669ad0d0151616
Juan Arcari <39750973+jsarcari@users.noreply.github.com>
2024-02-27 00:42:39 -0300

    print the genres for each movie

--- top-movies/src/components/ShowMovies.js
+++ top-movies/src/components/ShowMovies.js
@@ -1,11 +1,52 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function ShowMovies(props) {
+    const genreURL = "https://api.themoviedb.org/3/genre/movie/list?api_key=506fadb0256c13349acc05dabebf9604&language=en-US";
+
+    const [genre, setGenre] = useState(null);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        axios.get(genreURL).then((response) => {
+        setGenre(response.data);
+        }).catch(error => {
+        setError(error)
+        });
+    }, []);
+
+    if (error) return `Error: ${error.message}`;
+    if (!genre) return "No genres!";
+
+    var genresList = genre;
+
+    function printMovie(movie) {
+        return (
+        <tr key={movie.title}>
+            <th className="titleMovie"><img src={showBackdrop(movie.backdrop_path)} alt="" className="movie-backdrop"/><p className="title-movie">{`${movie.title}`}</p></th>
+            <td>
+                <div>{`${movie.overview}`}</div>
+                <br />
+                <div><strong>Genres:</strong> {movie.genre_ids.map(genre => printGenre(genre, movie))}</div>
+            </td>
+        </tr>
+        );
+    }
+
     function showBackdrop(backdrop) {
         return "https://image.tmdb.org/t/p/original" + backdrop;
     }
-    
+
+    function printGenre(idGenre, movie) {
+        var genreMovie = genresList.genres.find(genre => genre.id === idGenre);
+        if (movie.genre_ids.indexOf(idGenre) === movie.genre_ids.length-1) {
+            return `${genreMovie.name}`;
+        } else {
+            return `${genreMovie.name}, `;
+        }
+    }
+
     return (
     <div className="table table-dark table-striped">
       <thead className="headerTable">
@@ -15,10 +56,7 @@ export default function ShowMovies(props) {
         </tr>
       </thead>
       <tbody>
-          {props.array.map(item => <tr key={item.title}>
-                <th className="titleMovie"><img src={showBackdrop(item.backdrop_path)} alt="" className="movie-backdrop"/><p className="title-movie">{`${item.title}`}</p></th>
-                <td>{`${item.overview}`}</td>
-            </tr>)}
+          {props.array.map(item => printMovie(item))}
       </tbody>
     </div>
     );
