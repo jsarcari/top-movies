2836dc83bf100f719c716e1f9717489540ef1de3
Juan Arcari <39750973+jsarcari@users.noreply.github.com>
2024-03-04 04:03:53 -0300

    select order by

--- top-movies/src/components/ShowMovies.js
+++ top-movies/src/components/ShowMovies.js
@@ -22,6 +22,12 @@ export default function ShowMovies(props) {
 
     var genresList = genre;
     var valueGenre = props.value;
+    var orders = [
+        {value: 'alphabetical', label: 'A-Z'},
+        {value: 'alphabetical-desc', label: 'Z-A'},
+        {value: 'best-vote-average', label: 'Best vote average'},
+        {value: 'worst-vote-average', label: 'Worst vote average'}
+    ]
 
     function printMovie(movie) {
         return (<tr key={movie.title} className="row-movie">
@@ -96,29 +102,100 @@ export default function ShowMovies(props) {
         votes.innerHTML = movie.vote_average.toFixed(1)/2 + '/5.0';
     }
 
-    function getValueGenre() {
-        var index = document.getElementById("select-genre").value;
-        valueGenre = parseInt(index);
-        console.log(valueGenre);
-        var movies = document.querySelectorAll(".row-movie");
-        for(var i=0; i<movies.length; i++) {
-            movies[i].remove();
-        }
-        var table = document.querySelector(".list-movies");
-        for(var j=0; j<props.array.length; j++) {
-            const movie = props.array[j];
+    function constructTable(table) {
+        for(var w=0; w<props.array.length; w++) {
+            const movie = props.array[w];
             if(movie.genre_ids.includes(valueGenre) || valueGenre === 0) {
                 printMovieGenre(movie, table);
             }
         }
     }
 
+    function getValueGenre() {
+        var index = document.getElementById("select-genre").value;
+        valueGenre = parseInt(index);
+        var movies = document.querySelectorAll(".row-movie");
+        for(var i=0; i<movies.length; i++) {
+            movies[i].remove();
+        }
+        var table = document.querySelector(".list-movies");
+        constructTable(table);
+    }
+
+    function getOrder() {
+        var order = document.getElementById("select-order").value;
+        var movies = props.array;
+        var table = document.querySelector(".list-movies");
+        var rows = document.querySelectorAll(".row-movie");
+        switch(order) {
+            case 'alphabetical':
+                movies.sort(function(a, b) {
+                    if (a.title < b.title) {
+                        return -1;
+                    } else {
+                        return 1;
+                    }
+                });
+                for(var i=0; i<movies.length; i++) {
+                    props.array[i] = movies[i];
+                }
+                break;
+            case 'alphabetical-desc':
+                movies.sort(function(a, b) {
+                    if (a.title > b.title) {
+                        return -1;
+                    } else {
+                        return 1;
+                    }
+                });
+                for(var j=0; j<movies.length; j++) {
+                    props.array[j] = movies[j];
+                }
+                break;
+            case 'best-vote-average':
+                movies.sort(function(a, b) {
+                    if (a.vote_average > b.vote_average) {
+                        return -1;
+                    } else {
+                        return 1;
+                    }
+                });
+                for(var k=0; k<movies.length; k++) {
+                    props.array[k] = movies[k];
+                }
+                break;
+            case 'worst-vote-average':
+                movies.sort(function(a, b) {
+                    if (a.vote_average < b.vote_average) {
+                        return -1;
+                    } else {
+                        return 1;
+                    }
+                });
+                for(var l=0; l<movies.length; l++) {
+                    props.array[l] = movies[l];
+                }
+                break;
+        }
+        for(var m=0; m<rows.length; m++) {
+            rows[m].remove();
+        }
+        constructTable(table);
+    }
+
     return (
     <div className="table table-dark table-striped">
-      <select className="select" id="select-genre" name="select-genre" onChange={getValueGenre}>
-        <option value="0" selected>Select the genre</option>
-        {genresList.genres.map(genre => <option value={genre.id}>{genre.name}</option>)}
-      </select>
+        <div id="selects">
+            <label>Filters:</label>
+            <select className="select" id="select-genre" name="select-genre" onChange={getValueGenre}>
+                <option value="0" selected>Select the genre</option>
+                {genresList.genres.map(genre => <option value={genre.id}>{genre.name}</option>)}
+            </select>
+            <select className="select" id="select-order" name="select-order" onChange={getOrder}>
+                <option value="none" select>Order by</option>
+                {orders.map(order => <option value={order.value}>{order.label}</option>)}
+            </select>
+        </div>
       <thead>
         <tr>
           <th className="titleMovie" scope="col">Movie</th>
